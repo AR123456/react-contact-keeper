761cd4edbbb2043831de219e1284e5d8280de13f
AR123456 <coloradoz99@gmail.com>
2019-09-27 04:48:32 -0600

    express authentication

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,51 @@
+import React, { useReducer } from "react";
+
+import AuthContext from "./authContext";
+import authReducer from "./authReducer";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from "../types";
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+  ///pull out state and dispatch from reducer using reducer hooks
+  const [state, dispatch] = useReducer(authReducer, initialState);
+  // load user
+
+  // register user
+
+  // login user
+
+  //logout
+
+  //clear errors
+
+  //// return provider in order to wrap the application in this contexts
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthState;
