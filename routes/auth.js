ed4b280eab2fb75a0319d319d14fa724b3be57a8
AR123456 <coloradoz99@gmail.com>
2020-08-14 07:03:58 -0600

    backing out for now

--- routes/auth.js
+++ routes/auth.js
@@ -82,53 +82,54 @@ router.post(
 // @access    Public
 // TODO get the request from front end, if email in the DB generate a re set token
 // seems like this is a get first , then post
-router.post(
-  "/",
-  [check("email", "Please include a valid email").isEmail()],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { email } = req.body;
-    try {
-      let user = await User.findOne({ email });
+/// this did not work, will get signup confrimation working first
+// router.post(
+//   "/",
+//   [check("email", "Please include a valid email").isEmail()],
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+//     const { email } = req.body;
+//     try {
+//       let user = await User.findOne({ email });
 
-      if (!user) {
-        return res.status(400).json({ msg: "Invalid Credentials" });
-      }
-      crypto
-        .randomBytes(32, (err, buffer) => {
-          if (err) {
-            console.log(err);
-            // TODO  redirect to requestReset
-            // return res.redirect("/requestReset")
-          }
-          const token = buffer.toString("hex");
-          user.resetToken = token;
-          user.resetTokenExpiration = Date.now() + 3600000;
-          return user.save();
-        })
-        .then((result) => {
-          // we have a matching users and have given user token, saved it to the db
-          // now send email
-          res.redirect("/");
-          // TODO make this a dynamic link for reset
-          transporter.sendMail({
-            to: email,
-            from: "ContactKeeper.com",
-            subject: "Reset password has been requested ",
-            html: `
-            <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
-          `,
-          });
-        });
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
-);
+//       if (!user) {
+//         return res.status(400).json({ msg: "Invalid Credentials" });
+//       }
+//       crypto
+//         .randomBytes(32, (err, buffer) => {
+//           if (err) {
+//             console.log(err);
+//             // TODO  redirect to requestReset
+//             // return res.redirect("/requestReset")
+//           }
+//           const token = buffer.toString("hex");
+//           user.resetToken = token;
+//           user.resetTokenExpiration = Date.now() + 3600000;
+//           return user.save();
+//         })
+//         .then((result) => {
+//           // we have a matching users and have given user token, saved it to the db
+//           // now send email
+//           res.redirect("/");
+//           // TODO make this a dynamic link for reset
+//           transporter.sendMail({
+//             to: email,
+//             from: "ContactKeeper.com",
+//             subject: "Reset password has been requested ",
+//             html: `
+//             <p>You requested a password reset</p>
+//             <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+//           `,
+//           });
+//         });
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("Server Error");
+//     }
+//   }
+// );
 
 module.exports = router;
