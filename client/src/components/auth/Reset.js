5a39a472a49dff0e8d16c64d3e7adc0b88c929f3
AR123456 <coloradoz99@gmail.com>
2020-09-02 06:51:58 -0600

    moving to use state

--- client/src/components/auth/Reset.js
+++ client/src/components/auth/Reset.js
@@ -1,41 +1,50 @@
 // need to bring this component into App.js to see it
 import React, { useState, useContext, useEffect } from "react";
-
+import axios from "axios";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
 const Reset = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
-  //TODO
-  const { reset } = authContext;
+  //TODO going to try this with useState instead
+  // const { reset } = authContext;
 
   const { setAlert } = alertContext;
   //TODO seems like the use effect could be used to handle sending the token to
   // back end route to match up the token with what is in db
   // like matching up to valid email in the RequestReset
-  // put the token into state but
+  // put the reset token and password in to state
   // how to get the reset token off of the  URL  to send to back end to do the comparison??
-  //TODO in auth context make reset
-  //   const { register, error, clearErrors, isAuthenticated } = authContext;
 
-  //   useEffect(() => {
-  //     if (isAuthenticated) {
-  //       props.history.push("/");
-  //     }
+  // TODO google useEffect to make the axios request to get the token off url
+  //  React Axios Tutorial   https://www.youtube.com/watch?v=bYFYF2GnMy8
+  useEffect(() => {
+    axios
+      // 404 error with this
+      .get("/api/auth/reset/:token")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-  //     if (error === "User already exists") {
-  //       setAlert(error, "danger");
-  //       clearErrors();
-  //     }
-  //     // eslint-disable-next-line
-  //   }, [error, isAuthenticated, props.history]);
+    // eslint-disable-next-line
+  }, []);
   // because this is a form  use state to add the component level state
   const [user, setUser] = useState({
     // send in object that is state
     password: "",
     password2: "",
   });
+  const [resetToken, setResetToken] = useState({
+    // the token from the url
+    resetToken: "", // will be token
+  });
+  // TODO  make the setResetToken - it will get the token from the URL
+  // and pass it to  state to send to the back end
+
   // destructuring so that we can use as variables
   const { password, password2 } = user;
   // wireing up the onChange in the input's in the form groups
@@ -56,9 +65,12 @@ const Reset = (props) => {
     } else {
       //  TODO  this should be a method called reset , make this
       // in the context
-      reset({
-        password,
-      });
+      //TODO does this really need to be done with context ?
+      // TODO try re writing with useState since I just need to pass this to the back end.
+      // reset({
+      //   password,
+      // });
+      //TODO call the setResetToken function that sends the token and password to the back end
     }
   };
   return (
@@ -69,26 +81,6 @@ const Reset = (props) => {
       </h1>
       {/* form an onSubmit  with form groups that have labels and inputs, inputs will have an onChange   */}
       <form onSubmit={onSubmit}>
-        {/* <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}
-            required
-          />
-        </div> */}
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
